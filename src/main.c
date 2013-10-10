712255556b66292b72637f92946aed640b91c5b2
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-10-10 11:30:46 +0200

    Starting with reading the data file

--- src/main.c
+++ src/main.c
@@ -23,8 +23,64 @@ static NOINLINE int x264_pixel_satd_8x4( pixel *pix1, intptr_t i_pix1, pixel *pi
     return (((sum_t)sum) + (sum>>BITS_PER_SUM)) >> 1;
 }
 
-int main(char * argv)
+void help(char **argv)
 {
-    printf("hello world\n");
+    printf("Usage: %s <file>\n", argv[0]);
+}
+
+int read_pixels(FILE *fr, pixel *pix1, intptr_t *i_pix1, pixel *pix2, intptr_t *i_pix2)
+{
+    int w, v, x, y;
+
+    char buffer[20];
+    char c;
+    int i;
+    int j;
+
+    while ((c = getc(fr)) != EOF) {
+        if (c == '\n') {
+            i = 0;
+
+            sscanf(buffer, "(%d,%d)  %d %d", &w, &v, &x, &y);
+
+            (*i_pix1) = (intptr_t) w;
+            (*i_pix2) = (intptr_t) v;
+
+//            pix1[j] = x;
+//            pix2[j] = y;
+
+            // stop after 8 values
+            return ++j == 8;
+
+        } else if (i < 20) {
+            buffer[i++] = c;
+        }
+    }
+
+    return EOF;
+}
+
+int main(int argc, char **argv)
+{
+
+    if (argc < 2) {
+        help(argv);
+        return 1;
+    }
+
+    FILE *fr = fopen(argv[1], "r");
+
+    pixel *pix1;
+    intptr_t i_pix1;
+    pixel *pix2;
+    intptr_t i_pix2;
+
+    while (read_pixels(fr, pix1, &i_pix1, pix2, &i_pix2) != EOF) {
+        printf("%d %d\n", (int) i_pix1, (int) i_pix2);
+    }
+
+    fclose(fr);
+
+    printf("hello world %s\n", argv[0]);
     return 0;
 }
