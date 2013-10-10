8f1e9f931870353c19767830117facf28b40eba4
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-10-10 10:30:17 +0200

    Move some stuff for the x264_pixel_satd_8x4 function outside the main.c

--- src/main.c
+++ src/main.c
@@ -1,36 +1,6 @@
 
 #include <stdio.h>
-#include "osdep.h"
-#include "common.h"
-
-#if BIT_DEPTH > 8
-    typedef uint32_t sum_t;
-    typedef uint64_t sum2_t;
-#else
-    typedef uint16_t sum_t;
-    typedef uint32_t sum2_t;
-#endif
-
-#define BITS_PER_SUM (8 * sizeof(sum_t))
-
-#define HADAMARD4(d0, d1, d2, d3, s0, s1, s2, s3) {\
-    sum2_t t0 = s0 + s1;\
-    sum2_t t1 = s0 - s1;\
-    sum2_t t2 = s2 + s3;\
-    sum2_t t3 = s2 - s3;\
-    d0 = t0 + t2;\
-    d2 = t0 - t2;\
-    d1 = t1 + t3;\
-    d3 = t1 - t3;\
-}
-
-// in: a pseudo-simd number of the form x+(y<<16)
-// return: abs(x)+(abs(y)<<16)
-static ALWAYS_INLINE sum2_t abs2( sum2_t a )
-{
-    sum2_t s = ((a>>(BITS_PER_SUM-1))&(((sum2_t)1<<BITS_PER_SUM)+1))*((sum_t)-1);
-    return (a+s)^s;
-}
+#include "pixel.h"
 
 static NOINLINE int x264_pixel_satd_8x4( pixel *pix1, intptr_t i_pix1, pixel *pix2, intptr_t i_pix2 )
 {
