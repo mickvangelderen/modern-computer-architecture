2b5a3a635406b636b9d063ccbf62037bc2fd353f
Mick van Gelderen <mickvangelderen@gmail.com>
2013-10-15 23:39:07 +0200

    Working on adder

--- adder/src/main.c
+++ adder/src/main.c
@@ -8,6 +8,8 @@
 
 const char * const RVEX_CORE_CTL_FILE = "/sys/devices/virtual/rvex_core/rvex-smemory.0/rvex_core_ctl";
 const char * const RVEX_CORE_STATUS_FILE = "/sys/devices/virtual/rvex_core/rvex-smemory.0/rvex_core_status";
+const char * const RVEX_DATA_MEMORY_FILE = "/dev/rvex-dmemory.0";
+const char * const RVEX_INSTRUCTION_MEMORY_FILE = "/dev/rvex-imemory.0";
 
 // works like open(const char * path, int oflag, ...) but only supports path and flag parameters. Prints errors if any. 
 int myopen(const char * path, int oflag) {
@@ -40,19 +42,28 @@ int main(int argc, char **argv) {
 	int fd;
 	unsigned char status = 0;
 
+	char data[] = {0,1,2,3,4,5,6,7};
+
+	// Write some data to memory
+	fd = myopen(RVEX_DATA_MEMORY_FILE, O_WRONLY);
+	if (fd < 0) { return -1; }
+	write(fd, data, 8);
+	close(fd);
+	printf("Wrote {0,1,2,3,4,5,6,7} to \"%s\"\n", RVEX_DATA_MEMORY_FILE);
+
+	// Clear status and start procedure
 	fd = myopen(RVEX_CORE_CTL_FILE, O_WRONLY);
 	if (fd < 0) { return -1; }
-
 	write(fd, &RVEX_CLEAR, 1);
+	write(fd, &RVEX_START, 1);
 	close(fd);
-	printf("Wrote clear to \"%s\"", RVEX_CORE_CTL_FILE);
+	printf("Wrote clear and start to \"%s\"\n", RVEX_CORE_CTL_FILE);
 
 	fd = myopen(RVEX_CORE_STATUS_FILE, O_RDONLY);
 	if (fd < 0) { return -1; }
-
 	read(fd, &status, 1);
 	close(fd);
-	printf("Rvex_core_status is %d", status);
+	printf("Rvex_core_status is %d\n", status);
 
 	return 0;
 }
