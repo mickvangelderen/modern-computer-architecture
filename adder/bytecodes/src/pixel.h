0b79f766eb17d203f0aa8f6e3095d0a03823601f
Mick van Gelderen <mickvangelderen@gmail.com>
2013-10-31 13:08:04 +0100

    Bytecode for pixel_satd_8x4

---
+++ adder/bytecodes/src/pixel.h
@@ -0,0 +1,36 @@
+#ifndef MCA_PIXEL_H
+#define MCA_PIXEL_H
+
+typedef signed char     int8_t;
+typedef unsigned char   uint8_t;
+typedef signed short  int16_t;
+typedef unsigned short    uint16_t;
+typedef signed long int     int32_t;
+typedef unsigned long int   uint32_t;
+
+#define BIT_DEPTH 8
+
+#if BIT_DEPTH > 8
+    typedef uint32_t sum_t;
+    typedef uint64_t sum2_t;
+    typedef uint16_t pixel;
+#else
+    typedef uint16_t sum_t;
+    typedef uint32_t sum2_t;
+    typedef uint8_t  pixel;
+#endif
+
+#define BITS_PER_SUM (8 * sizeof(sum_t))
+
+#define HADAMARD4(d0, d1, d2, d3, s0, s1, s2, s3) {\
+    sum2_t t0 = s0 + s1;\
+    sum2_t t1 = s0 - s1;\
+    sum2_t t2 = s2 + s3;\
+    sum2_t t3 = s2 - s3;\
+    d0 = t0 + t2;\
+    d2 = t0 - t2;\
+    d1 = t1 + t3;\
+    d3 = t1 - t3;\
+}
+    
+#endif
---
+++ adder/bytecodes/src/x264.c
@@ -0,0 +1,32 @@
+#include "pixel.h"
+
+pixel pixels1[32];
+pixel pixels2[32];
+int result;
+
+sum2_t abs2(sum2_t a) {
+    sum2_t s = ((a>>(BITS_PER_SUM-1))&(((sum2_t)1<<BITS_PER_SUM)+1))*((sum_t)-1);
+    return (a+s)^s;
+}
+
+int main () {
+    sum2_t tmp[4][4];
+    sum2_t a0, a1, a2, a3;
+    sum2_t sum = 0;
+    pixel* pix1 = pixels1;
+    pixel* pix2 = pixels2;
+    int i;
+    for (i = 0; i < 4; i++, pix1 += 8, pix2 += 8) {
+        a0 = (pix1[0] - pix2[0]) + ((sum2_t)(pix1[4] - pix2[4]) << BITS_PER_SUM);
+        a1 = (pix1[1] - pix2[1]) + ((sum2_t)(pix1[5] - pix2[5]) << BITS_PER_SUM);
+        a2 = (pix1[2] - pix2[2]) + ((sum2_t)(pix1[6] - pix2[6]) << BITS_PER_SUM);
+        a3 = (pix1[3] - pix2[3]) + ((sum2_t)(pix1[7] - pix2[7]) << BITS_PER_SUM);
+        HADAMARD4(tmp[i][0], tmp[i][1], tmp[i][2], tmp[i][3], a0,a1,a2,a3);
+    }
+    for (i = 0; i < 4; i++) {
+        HADAMARD4(a0, a1, a2, a3, tmp[0][i], tmp[1][i], tmp[2][i], tmp[3][i]);
+        sum += abs2(a0) + abs2(a1) + abs2(a2) + abs2(a3);
+    }
+    result = (((sum_t)sum) + (sum>>BITS_PER_SUM)) >> 1;
+    return 0;
+}
